fedbd010ef9915c80d7435487d8e8a62c9d60c52
unknown <manoukyan.mariam@gmail.com>
2015-04-13 14:15:40 +0400

    first commit for this directory

---
+++ controllers/VideoadminController.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\controllers;
+
+use Yii;
+use app\models\Post;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+class VideoadminController extends Controller
+{
+    public function index(){
+
+        echo 'ddd';
+    }
+
+}
